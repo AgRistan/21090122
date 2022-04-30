90fc40e16a3877fdd019d8b38f79520e1c0fefbd
AgRistan <103366011+AgRistan@users.noreply.github.com>
2022-04-30 23:05:39 +0700

    Update perhitungan.java

--- perhitungan.java
+++ perhitungan.java
@@ -3,7 +3,7 @@ package perhi;
 public class perhitungan {
     int nilai;
     void hitung(){
-        nilai = 5+5;
+        nilai = 4+6;
         System.out.println("Hasil = " + nilai);
     }
     public static void main(String[] args) {
