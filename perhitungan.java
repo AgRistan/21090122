6ce51a59923e852c57343e9478ee4e49295ec890
AgRistan <103366011+AgRistan@users.noreply.github.com>
2022-04-30 23:04:13 +0700

    Perhi
    Tung

---
+++ perhitungan.java
@@ -0,0 +1,13 @@
+package perhi;
+
+public class perhitungan {
+    int nilai;
+    void hitung(){
+        nilai = 5+5;
+        System.out.println("Hasil = " + nilai);
+    }
+    public static void main(String[] args) {
+        perhitungan objek = new perhitungan();
+        objek.hitung();
+    }    
+}
